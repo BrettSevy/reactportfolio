85210464773c39b31cab3cc740f9724c32fe54b2
Brett Sevy <justbrettt@gmail.com>
2020-03-25 02:04:48 -0500

    Updates

---
+++ precache-manifest.6a6c06a11cf61b548ef285c963e6344f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcd8bf53005e27a3a84833e972c9f0e",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "2916c2032da539031390",
+    "url": "/reactportfolio/static/css/main.a47237d3.chunk.css"
+  },
+  {
+    "revision": "26fc22c366c6f4e41f62",
+    "url": "/reactportfolio/static/js/2.c61415a6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactportfolio/static/js/2.c61415a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2916c2032da539031390",
+    "url": "/reactportfolio/static/js/main.1e7fe0df.chunk.js"
+  },
+  {
+    "revision": "1f60bbe8a467a42401a1",
+    "url": "/reactportfolio/static/js/runtime-main.53cc0431.js"
+  },
+  {
+    "revision": "9b1d9f180e0dda24c7715a9009d5554b",
+    "url": "/reactportfolio/static/media/burgers.9b1d9f18.png"
+  },
+  {
+    "revision": "086246a59deea324a92cc52197b273cf",
+    "url": "/reactportfolio/static/media/coin.086246a5.jpg"
+  },
+  {
+    "revision": "8623edab5cad78c2e753474b3caa8dc9",
+    "url": "/reactportfolio/static/media/fitness.8623edab.png"
+  },
+  {
+    "revision": "742cefed97d3123c8e103f52190a2b33",
+    "url": "/reactportfolio/static/media/kobe.742cefed.png"
+  },
+  {
+    "revision": "f7c36b56a85c88c69192a8994b2deaef",
+    "url": "/reactportfolio/static/media/planner.f7c36b56.png"
+  },
+  {
+    "revision": "e44da7451f5e778fb0d7e5d73f1209e4",
+    "url": "/reactportfolio/static/media/stocks.e44da745.png"
+  },
+  {
+    "revision": "3cc4d84f594c7836caf6d2359a4f3c8d",
+    "url": "/reactportfolio/static/media/weather.3cc4d84f.png"
+  }
+]);
